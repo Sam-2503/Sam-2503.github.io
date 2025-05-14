3e747477a1b8d9006a65c59bf3edf9c145890b8b
Samprad <sampraaddas2503@gmail.com>
2025-05-14 13:57:29 +0530

    added script to scroll to clicked section

--- code/script.js
+++ code/script.js
@@ -1,11 +1,11 @@
-let homeButton = document.querySelector(".btn-home");
+let homeButton = document.querySelector(".homeButton");
 let homeContent  = document.querySelector(".home");
-homeButton.onclick = () => homeContent.scrollIntoView();
+homeButton.onclick = () => homeContent.scrollIntoView({behaviour: "smooth"});
 
-let contactButton = document.querySelector(".btn-contact");
+let contactButton = document.querySelector(".contactButton");
 let contactContent = document.querySelector(".contact");
-contactButton.onclick = () => contactContent.scrollIntoView();
+contactButton.onclick = () => contactContent.scrollIntoView({behaviour: "smooth"});
 
-let projectsButton = document.querySelector(".btn-projects");
+let projectsButton = document.querySelector(".projectsButton");
 let projectsContent = document.querySelector(".projects");
-projectsButton.onclick = () => projectsContent.scrollIntoView();
+projectsButton.onclick = () => projectsContent.scrollIntoView({behaviour: "smooth"});
