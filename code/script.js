ea1eb0907acf2f0479721b40e8957c2a9849c919
Samprad <sampraaddas2503@gmail.com>
2025-05-14 14:04:56 +0530

    fixed scrolling behavior

--- code/script.js
+++ code/script.js
@@ -1,11 +1,11 @@
 let homeButton = document.querySelector(".homeButton");
 let homeContent  = document.querySelector(".home");
-homeButton.onclick = () => homeContent.scrollIntoView({behaviour: "smooth"});
+homeButton.onclick = () => homeContent.scrollIntoView({behavior: "smooth"});
 
 let contactButton = document.querySelector(".contactButton");
 let contactContent = document.querySelector(".contact");
-contactButton.onclick = () => contactContent.scrollIntoView({behaviour: "smooth"});
+contactButton.onclick = () => contactContent.scrollIntoView({behavior: "smooth"});
 
 let projectsButton = document.querySelector(".projectsButton");
 let projectsContent = document.querySelector(".projects");
-projectsButton.onclick = () => projectsContent.scrollIntoView({behaviour: "smooth"});
+projectsButton.onclick = () => projectsContent.scrollIntoView({behavior: "smooth"});
