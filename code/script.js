165390c14e563a7af9c329c77112620a02d6f1b3
Samprad <sampraaddas2503@gmail.com>
2025-05-15 07:05:08 +0530

    scrolling function works properly now

--- code/script.js
+++ code/script.js
@@ -1,11 +1,31 @@
-let homeButton = document.querySelector(".homeButton");
-let homeContent  = document.querySelector(".home");
-homeButton.onclick = () => homeContent.scrollIntoView({behavior: "smooth"});
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarHeight = document.querySelector("nav").offsetHeight;
 
-let contactButton = document.querySelector(".contactButton");
-let contactContent = document.querySelector(".contact");
-contactButton.onclick = () => contactContent.scrollIntoView({behavior: "smooth"});
+    function scroller(section) {
+        const offset = navbarHeight-100;
+        const y = section.getBoundingClientRect().top + window.pageYOffset + offset;
+        window.scrollTo({
+            top: y,
+            behavior: "smooth"
+        });
+    }
 
-let projectsButton = document.querySelector(".projectsButton");
-let projectsContent = document.querySelector(".projects");
-projectsButton.onclick = () => projectsContent.scrollIntoView({behavior: "smooth"});
+    let homeButton = document.querySelector(".homeButton");
+    let homeContent  = document.querySelector(".home");
+    homeButton.onclick = function() {
+        scroller(homeContent);
+    }
+
+    let contactButton = document.querySelector(".contactButton");
+    let contactContent = document.querySelector(".contact");
+    contactButton.onclick = function() {
+        scroller(contactContent);
+    }
+
+    let projectsButton = document.querySelector(".projectsButton");
+    let projectsContent = document.querySelector(".projects");
+    projectsButton.onclick = function() {
+        scroller(projectsContent);
+    }
+
+});
