2cc1cd6a5d59994c333c5d7891e897f933720a50
Samprad <sampraaddas2503@gmail.com>
2025-08-27 01:00:09 +0530

    removed unncecessary files

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Bocy8eNu.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/Dn3YWNOJ.js","_app/immutable/chunks/BhQ-HJB1.js","_app/immutable/chunks/kOybiQqv.js","_app/immutable/chunks/6giCx1FX.js","_app/immutable/chunks/CzSflGjw.js"];
-export const stylesheets = [];
-export const fonts = [];
